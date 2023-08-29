c2b704bb90ceca54950953e04e38f123c739cf35
renasustek <renasustek@gmail.com>
2023-08-29 17:48:05 +0100

    made do make easier to read

--- src/pages/do/[title].js
+++ src/pages/do/[title].js
@@ -1,15 +1,12 @@
 import SingleDoMake from '../../components/domake/SingleDoMake'
 import Layout from '../../components/layout/Layout'
-import path from 'path'
-import { promises as fs } from 'fs'
+import { getStaticProps } from "../../lib/FetchData";
 
 export async function getServerSideProps({ params }) {
     // const domake = await fetchData(`do-make/?title=${params.title}`)
 
-    const jsonDirectory = path.join(process.cwd(), 'json')
-    //Read the json data file data.json
-    const fileContents = await fs.readFile(jsonDirectory + '/db.json', 'utf8')
-    const data = JSON.parse(fileContents)
+    const data = await getStaticProps();
+
     const domake = data['do-make'].filter(
         (domake) => domake.title === params.title
     )
